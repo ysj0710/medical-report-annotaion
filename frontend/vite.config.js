fd3e2c2b3da8fe3b3a71bb85a95aff2afdaf6a28
yuansijie <yuansijie@jvlei.com>
2026-03-24 17:17:35 +0800

    避免撞后端端口

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,13 +8,19 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8088',
         changeOrigin: true
       }
     }
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8088',
+        changeOrigin: true
+      }
+    }
   }
 })
